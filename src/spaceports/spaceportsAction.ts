56482d55be817124f4e33be9c91b1a91bd696787
Leandro Silva <grafluxe@users.noreply.github.com>
2021-02-05 02:26:03 -0500

    Prevent overlaps when adding a new spaceport

--- src/spaceports/spaceportsAction.ts
+++ src/spaceports/spaceportsAction.ts
@@ -1,7 +1,8 @@
 import { Maybe } from "graphql/jsutils/Maybe";
+import { UserInputError } from "apollo-server";
 import spaceports from "./spaceports.json";
 import { Port as Spaceport } from "../shared/types";
-import { findPort } from "../shared/portsAction";
+import { findPort, getPortOverlaps } from "../shared/portsAction";
 
 const findSpaceport = findPort(spaceports);
 
@@ -11,6 +12,17 @@ export const getSpaceport = (search: Partial<Spaceport>): Maybe<Spaceport> =>
   findSpaceport(search);
 
 export const addSpaceport = (spaceport: Spaceport): Spaceport => {
+  const getSpaceportOverlaps = getPortOverlaps(spaceport);
+  const spaceportOverlaps = spaceports.reduce(getSpaceportOverlaps, []);
+
+  if (spaceportOverlaps.length > 0) {
+    const fields = spaceportOverlaps.join(" & ");
+
+    throw new UserInputError(
+      `The ${fields} you've entered has already been assigned to a spaceport`
+    );
+  }
+
   // eslint-disable-next-line fp/no-mutating-methods
   spaceports.push(spaceport);
   return spaceport;
